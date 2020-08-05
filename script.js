e64b719d5b1b52adbbaba7f30294d1c35495cb5d
LitleStuart <mukhorinvanga@gmail.com>
2020-08-06 00:36:33 +0500

    default dark theme and bug fix

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const changeTheme = document.getElementById('changeTheme');
 const theme = document.getElementById('style');
-let darkThemeEnabled = false;
+const keksImage = document.getElementById('keksImage');
+let darkThemeEnabled = true;
 
 changeTheme.addEventListener('click', () => {
     if (darkThemeEnabled) {
         theme.href = 'style.css';
+        keksImage.setAttribute('src', 'files/project.jpg');
         darkThemeEnabled = false;
     } else {
         theme.href = 'dark-theme/style.css';
+        keksImage.setAttribute('src', '../files/project1.jpg');
         darkThemeEnabled = true;
     }
 })
